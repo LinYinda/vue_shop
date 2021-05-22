fe288d6089b60420e9579d363763522e140d4c68
LinYinda <1067922218@qq.com>
2021-05-22 19:02:26 +0800

    登录完成

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,9 +11,7 @@ module.exports = {
     parser: 'babel-eslint'
   },
   rules: {
-    'no-console':  'off',
-    'no-irregular-whitespace':'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'space-before-function-paren':0
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   }
 }
