763592bfb1bebc45431ee40b2671ec4a230aa513
LinYinda <1067922218@qq.com>
2021-05-26 11:15:34 +0800

    商品分类v1.0

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
 import './assets/fonts/iconfont.css'
+import TreeTable from 'vue-table-with-tree-grid'
 
 import axios from 'axios'
 axios.defaults.baseURL = 'http://www.ysqorz.top:8888/api/private/v1/'
@@ -16,6 +17,8 @@ Vue.prototype.$http =axios;
 import './assets/css/global.css'
 
 Vue.config.productionTip = false
+
+Vue.component('tree-table',TreeTable);
 
 new Vue({
   router,
