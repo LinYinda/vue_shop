a90478c515aecff1fc2e111f74b74f9b1137f649
LinYinda <1067922218@qq.com>
2021-05-27 10:42:50 +0800

    分类参数v1.0

--- src/components/goods/Params.vue
+++ src/components/goods/Params.vue
@@ -29,7 +29,14 @@
                         <!--展开行-->
                         <el-table-column type="expand" >
                             <template v-slot="scope">
-                                <el-tag v-for="(item,i) in scope.row.attr_vals" :key="i">{{item}}</el-tag>
+                                <!--循环渲染tag标签-->
+                                <el-tag v-for="(item,i) in scope.row.attr_vals" :key="i" closable @close="handleClose(i,scope.row)">{{item}}</el-tag>
+                                <!--输入文本框-->
+                                <el-input class="input-new-tag"  v-if="scope.row.inputVisible" v-model="scope.row.inputValue"
+                                ref="saveTagInput" size="small"  @keyup.enter.native="handleInputConfirm(scope.row)" @blur="handleInputConfirm(scope.row)" >
+                                </el-input>
+                                <!--添加按钮-->
+                                <el-button v-else class="button-new-tag" size="small" @click="showInput(scope.row)">+ New Tag</el-button>
                             </template>
                         </el-table-column>
                         <!--索引行-->
@@ -51,7 +58,14 @@
                         <!--展开行-->
                         <el-table-column type="expand" >
                             <template v-slot="scope">
-                                <el-tag v-for="(item,i) in scope.row.attr_vals" :key="i" closable>{{item}}</el-tag>
+                                <!--循环渲染tag标签-->
+                                <el-tag v-for="(item,i) in scope.row.attr_vals" :key="i" closable @close="handleClose(i,scope.row)">{{item}}</el-tag>
+                                <!--输入文本框-->
+                                <el-input class="input-new-tag"  v-if="scope.row.inputVisible" v-model="scope.row.inputValue"
+                                ref="saveTagInput" size="small"  @keyup.enter.native="handleInputConfirm(scope.row)" @blur="handleInputConfirm(scope.row)" >
+                                </el-input>
+                                <!--添加按钮-->
+                                <el-button v-else class="button-new-tag" size="small" @click="showInput(scope.row)">+ New Tag</el-button>
                             </template>
                         </el-table-column>
                         <!--索引行-->
@@ -173,6 +187,7 @@ export default {
         //级联选择框选择变化
         handleChange(){
             this.getParamsData();
+
         },
         //tab页签点击事件处理
         handleTabClick(){
@@ -182,6 +197,8 @@ export default {
         async getParamsData(){
             if(this.selectedCateKeys.length !== 3){
                 this.selectedCateKeys = [];
+                this.manyTableData = [];
+                this.onlyTableData = [];
                 return;
             }
             //根据所选分类ID获取对应参数
@@ -190,8 +207,10 @@ export default {
 
             res.data.forEach(item => {
                 item.attr_vals =  item.attr_vals ? item.attr_vals.split(' ') : [];
+                //单独控制每个文本框显示与隐藏
+                item.inputVisible = false;
+                item.inputValue = '';
             });
-            console.log(res.data);
             if(this.activeName ==='many'){
                 this.manyTableData = res.data;
             }
@@ -248,6 +267,42 @@ export default {
             this.$message.success('删除参数成功！');
             //刷新列表数据
             this.getParamsData();
+        },
+        //文本框失去焦点或者按下回车
+        handleInputConfirm(row){
+            if(row.inputValue.trim().length == 0 ){
+                row.inputValue = '';
+                row.inputVisible = false;
+                return 
+            }
+            //用户输入内容做后续处理
+            row.attr_vals.push(row.inputValue.trim());
+            row.inputValue = '';
+            row.inputVisible = false;
+            
+            this.saveAttrVals(row);
+        },
+        //点击按钮，展示文本输入框
+        //this.$nextTick作用是当页面元素被重新渲染的时候，才会执行回调函数中的代码
+        showInput(row){
+            row.inputVisible = true;
+            this.$nextTick(_ => {
+          this.$refs.saveTagInput.$refs.input.focus();
+        });
+        },
+        //删除参数
+        handleClose(i, row){
+            //从第i项开始删除1项
+            row.attr_vals.splice(i,1);
+            this.saveAttrVals(row);
+
+        },
+        //发起请求
+        async saveAttrVals(row){
+            //join是拼接
+            const {data:res} = await this.$http.put(`categories/${this.cateId}/attributes/${row.attr_id}`,{attr_name:row.attr_name , attr_sel:row.attr_sel, attr_vals:row.attr_vals.join(' ')});
+            if(res.meta.status !==200 ) return this.$message.error('修改参数失败！');
+            this.$message.success('修改参数成功！');
         }
     }
 }
@@ -259,5 +314,8 @@ export default {
 }
 .el-tag{
     margin:10px;
+}
+.input-new-tag{
+    width:120px;
 }
 </style>
